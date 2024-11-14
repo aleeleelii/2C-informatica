c0b1883abc283a276b69ea8cc68a2a2aad1f4487
aleeleelii <elisa.cannata@iismontalegenova.it>
2024-11-14 10:45:31 +0100

    js

---
+++ programma.js
@@ -0,0 +1,27 @@
+
+
+let contenitore = document.getElementById("contenitore");
+contenitore.style.borderStyle = "solid" ;
+
+let elemento = document.createElement("div");
+elemento.style.borderStyle = "solid"
+elemento.style.width = "70px"
+elemento.style.textAlign = "center"
+elemento.style.position = "absolute"
+elemento .style.top = "500px" ;
+elemento.style.left = "500px" ;
+
+let numeroAtomico = document.createElement("p")
+numeroAtomico.innerText = 1 ;
+elemento.appendChild(numeroAtomico)
+
+let simbolo = document.createElement("a")
+simbolo.innerText = "H" ;
+simbolo.href = "https://it.wikipedia.org/wiki/Idrogeno"
+elemento.appendChild(simbolo)
+
+let nome = document.createElement("p")
+nome.innerText = "idrogeno" ;
+elemento.appendChild(nome)
+
+contenitore.appendChild(elemento)
