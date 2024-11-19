2a7ae1e2b75d1f332f24312a0bb12701e4a638f1
aleeleelii <elisa.cannata@iismontalegenova.it>
2024-11-19 10:50:26 +0100

    abbiamo ingrandito

--- programma.js
+++ programma.js
@@ -1,27 +1,40 @@
 
 
-let contenitore = document.getElementById("contenitore");
-contenitore.style.borderStyle = "solid" ;
+for (const element of elementi) { 
+    let contenitore = document.getElementById("contenitore");
+contenitore.style.position = "relative"
 
-let elemento = document.createElement("div");
-elemento.style.borderStyle = "solid"
-elemento.style.width = "70px"
-elemento.style.textAlign = "center"
-elemento.style.position = "absolute"
-elemento .style.top = "500px" ;
-elemento.style.left = "500px" ;
+    
+    let elemento = document.createElement("div");
+    elemento.style.borderStyle = "solid"
+    elemento.style.width = "90px"
+    elemento.style.height = "120px"
+    elemento.style.textAlign = "center"
+    elemento.style.position = "absolute"; 
+    elemento.style.left = 90 * (element.xpos -1) + "px";
+    elemento.style.top = 120 * (element.ypos -1) + "px";
+    elemento.style.backgroundColor = "#" + element ["cpk-hex"];
+    
+    
+    let numeroAtomico = document.createElement("p")
+    numeroAtomico.style.textAlign = "left"
+    numeroAtomico.innerText = element.number;
+    elemento.appendChild(numeroAtomico)
+    
+    let simbolo = document.createElement("a")
+    simbolo.innerText = element.symbol;
+    simbolo.href = element.source;
+    elemento.appendChild(simbolo)
+    simbolo.style.textDecoration = "none"
+simbolo.style.fontSize = "25px"
+simbolo.style.frontwait
 
-let numeroAtomico = document.createElement("p")
-numeroAtomico.innerText = 1 ;
-elemento.appendChild(numeroAtomico)
-
-let simbolo = document.createElement("a")
-simbolo.innerText = "H" ;
-simbolo.href = "https://it.wikipedia.org/wiki/Idrogeno"
-elemento.appendChild(simbolo)
-
-let nome = document.createElement("p")
-nome.innerText = "idrogeno" ;
-elemento.appendChild(nome)
-
-contenitore.appendChild(elemento)
+    
+    let nome = document.createElement("p")
+    nome.innerText = element.name;
+    elemento.appendChild(nome)
+    
+    contenitore.appendChild(elemento)
+    
+    
+}
